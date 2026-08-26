51257a3d4d5bcc452d68d8d4c14bba0a202e2c50
Prathmesh <prathmesh@example.com>
2026-08-26 10:36:04 +0530

    auto create database on startup

--- app.py
+++ app.py
@@ -268,6 +268,12 @@ def logout():
 
 import os
 
+with app.app_context():
+    db.create_all()
+    from models import Product
+    if Product.query.count() == 0:
+        import add_data
+        
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
